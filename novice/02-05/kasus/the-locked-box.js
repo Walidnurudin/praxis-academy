3b2f07366cb11aceccd02f463eee90b05e8676b4
Walid nurudin <walidnurudin47@gmail.com>
2020-08-31 15:32:11 +0700

    done

---
+++ novice/02-05/kasus/the-locked-box.js
@@ -0,0 +1,44 @@
+const box = {
+  locked: true,
+  unlock() { this.locked = false; },
+  lock() { this.locked = true;  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  // Your code here.
+  let waslocked = box.locked;
+  box.unlock();
+  try {
+    body();
+  }finally{
+    if(waslocked){
+      console.log(box.content);
+      box.lock();
+    }
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised: " + e.message);
+}
+console.log(box.locked);
+// → true
+
+
+
+
+
+
