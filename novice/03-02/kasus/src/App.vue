11db723b47cda30b5e8d8e3d985e77f4a2af2f25
Walid nurudin <walidnurudin47@gmail.com>
2020-09-02 21:08:54 +0700

    clear

---
+++ novice/03-02/kasus/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div id="demo">
+      <form id="search">
+        Search
+        <input name="query" v-model="searchQuery" />
+      </form>
+      <Grid :heroes="gridData" :columns="gridColumns" :filter-key="searchQuery"></Grid>
+    </div>
+    <div id="app">
+      <button id="show-modal" @click="showModal = true">Show Modal</button>
+      <!-- use the modal component, pass in the prop -->
+      <Modal v-if="showModal" @close="showModal = false">
+        <!--
+      you can use custom content here to overwrite
+      default content
+        -->
+        <h3 slot="header">custom header</h3>
+      </Modal>
+    </div>
+  </div>
+</template>
+
+<script>
+import Grid from "./components/grid.vue";
+import Modal from "./components/modal"
+
+export default {
+  name: "App",
+  components: {
+    Grid,
+    Modal
+  },
+  data: function() {
+    return {
+      showModal: false,
+      searchQuery: "",
+      gridColumns: ["name", "power"],
+      gridData: [
+        { name: "Chuck Norris", power: Infinity },
+        { name: "Bruce Lee", power: 9000 },
+        { name: "Jackie Chan", power: 7000 },
+        { name: "Jet Li", power: 8000 }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
