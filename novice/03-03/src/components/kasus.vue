acc1d930256db447813e5fc900e1a376381c707b
Walid nurudin <walidnurudin47@gmail.com>
2020-09-03 16:45:44 +0700

    03-03

--- novice/03-03/src/components/kasus.vue
+++ novice/03-03/src/components/kasus.vue
@@ -1,52 +1,66 @@
 <template>
   <div>
     <h1>kasus</h1>
-    <p v-for="item in data" :key="item.id">{{item.id}}. {{item.title}}</p>
+    <table>
+      <tr>
+        <th>No.</th>
+        <th>Title</th>
+      </tr>
+      <tr v-for="item in data" :key="item.id">
+        <td>{{item.id}}.</td>
+        <td>{{item.title}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-    name: "kasus",
-    data: () => {
-      return {
-        data: ''
-      }
-    },
-    beforeCreate(){
-      console.log("Before create!");
-    },
-    created(){
-      console.log("Created!");
-    },
-    beforeMount(){
-      console.log("Before mount!");
-      fetch('https://jsonplaceholder.typicode.com/todos/')
-        .then(res => res.json())
-        .then(res => {
-          this.data = res;
-          console.log(this.data);
-        })
-        .catch(err => console.log(err))
-    },
-    mounted(){
-      console.log("Mounted!")
-    },
-    beforeUpdate(){
-      console.log("Before update!");
-    },
-    updated(){
-      console.log("Update!")
-    },
-    beforeDestroy(){
-      console.log("Before destroyed!")
-    },
-    destroyed(){
-      console.log("Destroyed!")
-    }
-}
+  name: "kasus",
+  data: () => {
+    return {
+      data: ""
+    };
+  },
+  beforeCreate() {
+    console.log("Before create!");
+  },
+  created() {
+    console.log("Created!");
+    fetch("https://jsonplaceholder.typicode.com/todos/")
+      .then(res => res.json())
+      .then(res => {
+        this.data = res;
+        console.log(this.data);
+      })
+      .catch(err => console.log(err));
+  },
+  beforeMount() {
+    console.log("Before mount!");
+  },
+  mounted() {
+    console.log("Mounted!");
+  },
+  beforeUpdate() {
+    console.log("Before update!");
+  },
+  updated() {
+    console.log("Update!");
+  },
+  beforeDestroy() {
+    console.log("Before destroyed!");
+  },
+  destroyed() {
+    console.log("Destroyed!");
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
+table {
+  margin: 0 auto;
+}
+table, tr, th, td {
+  border: 1px solid black;
+}
 </style>
