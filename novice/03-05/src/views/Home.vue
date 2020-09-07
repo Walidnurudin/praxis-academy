e4ef03f10103aa091075772a17d0684f09437b55
Walid nurudin <walidnurudin47@gmail.com>
2020-09-07 13:53:05 +0700

    03-05

--- novice/03-05/src/views/Home.vue
+++ novice/03-05/src/views/Home.vue
@@ -1,9 +1,27 @@
 <template>
     <v-container>
       <v-row>
-        <v-col cols="6">
+        <v-col align="center">
           <v-card outlined>
-            <v-icon>mdi-abjad-arabic</v-icon>
+            <h1>JSON placeholder | POSTS</h1>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col>
+          <v-card outlined>
+            <h1>category</h1>
+            <div v-for="item in data" :key="item.id">
+              <p>{{item.id}}. {{item.title}}</p>
+            </div>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card outlined>
+            <h1>content</h1>
+            <div v-for="item in data" :key="item.id">
+              <p>{{item.id}}. {{item.body}}</p>
+            </div>
           </v-card>
         </v-col>
       </v-row>
@@ -15,6 +33,17 @@
 
 export default {
   name: "Home",
-  components: {}
+  data: () => {
+    return {
+      data: []
+    }
+  },
+  components: {},
+  async mounted(){
+    let data = await fetch('http://jsonplaceholder.typicode.com/posts')
+                        .then(res => res.json());
+    this.data = data;
+    console.log(this.data);
+  }
 };
 </script>
