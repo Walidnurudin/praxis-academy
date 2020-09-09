801936e9865b0c1ff0bf79181b4b70b434caafa3
Walid nurudin <walidnurudin47@gmail.com>
2020-09-09 10:52:54 +0700

    04-02

---
+++ novice/04-02/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-text-field type="text" v-model="todo" @keyup.enter="add()" label="masukan data"></v-text-field>
+    <v-btn outlined @click="add()">add</v-btn>
+    
+    <div v-for="(a, i) in data" :key="i">
+      <h1>{{a.title}}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  data: ()=> {
+    return {
+      todo: '',
+      data: []
+    }
+  },
+  methods: {
+    add: function(){
+      this.data.push({title: this.todo});
+      this.todo = '';
+    }
+  }
+}
+</script>
